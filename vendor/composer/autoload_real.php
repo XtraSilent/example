054379e4587b51564c2cd4e97dd7543a72200cfb
alif <alif@easycrypto.ai>
2021-02-27 08:10:12 +0800

    .

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInit105f2aba3cbb5514ac3c2c84818f0f81
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit105f2aba3cbb5514ac3c2c84818f0f81', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInit105f2aba3cbb5514ac3c2c84818f0f81
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit105f2aba3cbb5514ac3c2c84818f0f81::getInitializer($loader));
         } else {
